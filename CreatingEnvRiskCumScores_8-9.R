a8437202aee7604eb66e57ebed03a0599666bbb0
Vilte Baltramonaityte <baltramonaityte.vilte@gmail.com>
2021-02-14 15:10:18 +0200

    updating prenatal and 8-9 years sripts

--- CreatingEnvRiskCumScores_8-9.R
+++ CreatingEnvRiskCumScores_8-9.R
@@ -7,64 +7,60 @@
 
 
 # 1. LIFE EVENTS
-LE_interview <- readquick("insert ALSPAC data") # insert nr of obs and nr of columns
 
-# Exclude unreliable interviews
-LE_interview <- subset(LE_interview, LE_interview$unreliable == 0) # need to see ALSPAC data for the corresponding code here
+# Read in the data
+# Exclude unreliable interviews (not sure if this is relvant for ALSPAC)
 
-# Select the necessary item 
-# LE_interview to be replaced with ALSPAC equivalent
+# CS_LE_9Y = cumulative score for life events at 9 years (mother-based)
 
-#CS_LE_9Y = cumulative score for life events at 9 years (mother-based)
-
-CS_LE_9Y <- data.frame(LE_interview$?, # child id to be added 
-                       LE_interview$p2000_rec, # Husband/partner died since the study child's 6th birthday 
-                       LE_interview$p2001_rec, # One of mother's children died since the study child's 6th birthday
-                       LE_interview$p2002_rec, # Mother's friend or relative died since the study child's 6th birthday
-                       LE_interview$p2003_rec, # One of mother's children was ill since the study child's 6th birthday
-                       LE_interview$p2004_rec, # Mother's husband/partner was ill since the study child's 6th birthday
-                       LE_interview$p2005_rec, # Mother's friend or relative was ill since the study child's 6th birthday
-                       LE_interview$p2006_rec, # Mother was admitted to hospital since the study child's 6th birthday
-                       LE_interview$p2010_rec, # Mother was very ill since the study child's 6th birthday
-                       LE_interview$p2011_rec, # Mother's husband/partner lost his job since the study child's 6th birthday
-                       LE_interview$p2012_rec, # Mother's husband/partner had problems at work since the study child's 6th birthday
-                       LE_interview$p2013_rec, # Mother had problems at work since the study child's 6th birthday
-                       LE_interview$p2014_rec, # Mother lost her job since the study child's 6th birthday
-                       LE_interview$p2021_rec, # Mother moved house since the study child's 6th birthday
-                       LE_interview$p2030_rec, # Mother became pregnant since the study child's 6th birthday
-                       LE_interview$p2031_rec, # Mother started a new job since the study child's 6th birthday
-                       LE_interview$p2032_rec, # Mother returned to work since the study child's 6th birthday
-                       LE_interview$p2033_rec, # Mother had a miscarriage since the study child's 6th birthday
-                       LE_interview$p2035_rec, # Mother took an examination since the study child's 6th birthday
-                       LE_interview$p2039_rec, # Mother's house or car was burgled since the study child's 6th birthday
-                       LE_interview$p2041_rec, # One of mother's children started school since the study child's 6th birthday
-                       LE_interview$p2042_rec, # Mother's husband/partner started a new job since the study child's 6th birthday
-                       LE_interview$p2043_rec, # A pet died since the study child's 6th birthday
-                       LE_interview$p2044_rec) # Mother had an accident since the study child's 6th birthday
+CS_LE_9Y <- data.frame(?, # child id to be added 
+                       p2000_rec, # Husband/partner died since the study child's 6th birthday 
+                       p2001_rec, # One of mother's children died since the study child's 6th birthday
+                       p2002_rec, # Mother's friend or relative died since the study child's 6th birthday
+                       p2003_rec, # One of mother's children was ill since the study child's 6th birthday
+                       p2004_rec, # Mother's husband/partner was ill since the study child's 6th birthday
+                       p2005_rec, # Mother's friend or relative was ill since the study child's 6th birthday
+                       p2006_rec, # Mother was admitted to hospital since the study child's 6th birthday
+                       p2010_rec, # Mother was very ill since the study child's 6th birthday
+                       p2011_rec, # Mother's husband/partner lost his job since the study child's 6th birthday
+                       p2012_rec, # Mother's husband/partner had problems at work since the study child's 6th birthday
+                       p2013_rec, # Mother had problems at work since the study child's 6th birthday
+                       p2014_rec, # Mother lost her job since the study child's 6th birthday
+                       p2021_rec, # Mother moved house since the study child's 6th birthday
+                       p2030_rec, # Mother became pregnant since the study child's 6th birthday
+                       p2031_rec, # Mother started a new job since the study child's 6th birthday
+                       p2032_rec, # Mother returned to work since the study child's 6th birthday
+                       p2033_rec, # Mother had a miscarriage since the study child's 6th birthday
+                       p2035_rec, # Mother took an examination since the study child's 6th birthday
+                       p2039_rec, # Mother's house or car was burgled since the study child's 6th birthday
+                       p2041_rec, # One of mother's children started school since the study child's 6th birthday
+                       p2042_rec, # Mother's husband/partner started a new job since the study child's 6th birthday
+                       p2043_rec, # A pet died since the study child's 6th birthday
+                       p2044_rec) # Mother had an accident since the study child's 6th birthday
 
 
 colnames(CS_LE_9Y) <- c("IDC","husband/partner_died","m_children_died","m_friend/relative_died","m_children_ill","m_friend/relative_ill","m_admitted_hospital","m_very_ill",
                            "m_husband/partner_lost_job","m_husband/partner_problems_work","m_problems_work ", "m_lost_job","m_moved_house","m_became_pregnant","m_new_job",
                            "m_returned_work","m_miscarriage","m_examination","m_house/car_burgled","m_child_start_school","m_husband/partner_new_job","pet_died",
-                           "m_accident")
+                           "m_accident") # provide meaningful names for each variable 
 
 
 # CS_LEChild_9Y = cumulative score for life events at 9 years (child-based)
 
 CS_LEChild_9Y <- data.frame(kt5000a_rec, # Since 7th birthday child has been taken into care
-                                    kt5001a_rec, # Since 7th birthday child's pet died
-                                    kt5002a_rec, # Since 7th birthday child moved home
-                                    kt5003a_rec, # Since 7th birthday child had shock or fright
-                                    kt5006a_rec, # Since 7th birthday child has had someone in family die
-                                    kt5007a_rec, # Since 7th birthday child has been separated from mother
-                                    kt5008a_rec, # Since 7th birthday child has been separated from mother
-                                    kt5009a_rec, # Since 7th birthday child has been separated from father
-                                    kt5010a_rec, # Since 7th birthday child has had a new mother or father
-                                    kt5011a_rec, # Since 7th birthday child has had a new brother or sister
-                                    kt5012a_rec, # Since 7th birthday child has been admitted to hospital
-                                    kt5013a_rec, # Since 7th birthday child has been separated from someone else
-                                    kt5014a_rec, # Since 7th birthday child has started a new school
-                                    kt5015a_rec) # Since 7th birthday child has lost their best friend)
+                            kt5001a_rec, # Since 7th birthday child's pet died
+                            kt5002a_rec, # Since 7th birthday child moved home
+                            kt5003a_rec, # Since 7th birthday child had shock or fright
+                            kt5006a_rec, # Since 7th birthday child has had someone in family die
+                            kt5007a_rec, # Since 7th birthday child has been separated from mother
+                            kt5008a_rec, # Since 7th birthday child has been separated from mother
+                            kt5009a_rec, # Since 7th birthday child has been separated from father
+                            kt5010a_rec, # Since 7th birthday child has had a new mother or father
+                            kt5011a_rec, # Since 7th birthday child has had a new brother or sister
+                            kt5012a_rec, # Since 7th birthday child has been admitted to hospital
+                            kt5013a_rec, # Since 7th birthday child has been separated from someone else
+                            kt5014a_rec, # Since 7th birthday child has started a new school
+                            kt5015a_rec) # Since 7th birthday child has lost their best friend)
 
 
 ####################################################################################################################################################
@@ -74,8 +70,8 @@ CS_LEChild_9Y <- data.frame(kt5000a_rec, # Since 7th birthday child has been tak
 # CS_8_10_ContextualRisk = cumulative score from 8-10 years for contextual risk 
 
 CS_8_10_ContextualRisks <- data.frame(p2018_rec, # Mother's income was reduced since the study child's 6th birthday
-                                     p2023_rec, # Mother became homeless since the study child's 6th birthday
-                                     p2024_rec) # Mother had a major financial problem since the study child's 6th birthday
+                                      p2023_rec, # Mother became homeless since the study child's 6th birthday
+                                      p2024_rec) # Mother had a major financial problem since the study child's 6th birthday
 
 ####################################################################################################################################################
 
--- CreatingEnvRiskCumScores_Prenatal.R
+++ CreatingEnvRiskCumScores_Prenatal.R
@@ -11,7 +11,8 @@
 # depending on the format of the data, you may want to write "attach(nameofdataframe)" and then use the code below. 
 # This will permit the variables to be accessed directly without having to use the dollar sign, as here: 'nameofdata$variablename'
 
-CS_Prenatal_LifeEvents <- data.frame(b570a_rec, # PTNR died since PREG
+CS_Prenatal_LifeEvents <- data.frame(IDM, # add mothers ID here (find the equivalent term within ALSPAC)
+                                     b570a_rec, # PTNR died since PREG
                                      b571a_rec, # CH died since PREG
                                      b572a_rec, # Friend or relative died since PREG
                                      b573a_rec, # CH was ill since PREG	
@@ -40,7 +41,8 @@ CS_Prenatal_LifeEvents <- data.frame(b570a_rec, # PTNR died since PREG
 # 2. CONTEXTUAL RISKS
 
 
-CS_Prenatal_ContextualRisks  <- data.frame(b588a_rec, # Income reduced since PREG
+CS_Prenatal_ContextualRisks  <- data.frame(IDM, # add mothers ID here,
+                                           b588a_rec, # Income reduced since PREG
                                            b593a_rec, # Became homeless since PREG
                                            b594a_rec, # Major financial PROB since PREG
                                            p2, # Housing adequacy 
@@ -55,21 +57,24 @@ CS_Prenatal_ContextualRisks  <- data.frame(b588a_rec, # Income reduced since PRE
 
 
 
-CS_Prenatal_MH_LifestyleRisks  <- data.frame(b577a_rec, # In trouble with the law since PREG
-                                             b586a_rec, # PTNR in trouble with law since PREG
-                                             b597a_rec, # Attempted suicide since PREG
-                                             b598a_rec, # Convicted of an offence since PREG
-                                             b605a_rec, # Tried to have abortion
-                                             p1, # Early parenthood
-                                             p5, # Maternal education 
-                                             p12, # Psychopathology of mother
-                                             p13, # Substance abuse 
-                                             p14, # Crime trouble with police 
-                                             p15) # Crime convictions   
-                                            # This first version appears in CumRisk_Instructions&Information.xlsx, 
-                                            # likely suggesting from the three version, this one was selected to form the cumulative score)
+CS_Prenatal_ParentalRisks  <- data.frame(IDM, # add mothers ID here,
+                                         b577a_rec, # In trouble with the law since PREG
+                                         b586a_rec, # PTNR in trouble with law since PREG
+                                         b597a_rec, # Attempted suicide since PREG
+                                         b598a_rec, # Convicted of an offence since PREG
+                                         b605a_rec, # Tried to have abortion
+                                         p1, # Early parenthood
+                                         p5, # Maternal education 
+                                         p12, # Psychopathology of mother
+                                         p13, # Substance abuse 
+                                         p14, # Crime trouble with police 
+                                         p15) # Crime convictions   
+# This first version appears in CumRisk_Instructions&Information.xlsx, 
+# likely suggesting from the three version, this one was selected to form the cumulative score)
 
-CS_Prenatal_Parental_NoSubUse <- data.frame(b577a_rec, # In trouble with the law since PREG
+                                            
+CS_Prenatal_Parental_NoSubUse <- data.frame(IDM, # add mothers ID here,
+                                            b577a_rec, # In trouble with the law since PREG
                                             b586a_rec, # PTNR in trouble with law since PREG
                                             b597a_rec, # Attempted suicide since PREG
                                             b598a_rec, # Convicted of an offence since PREG
@@ -83,7 +88,8 @@ CS_Prenatal_Parental_NoSubUse <- data.frame(b577a_rec, # In trouble with the law
 # The difference between CS_Prenatal_MH_LifestyleRisks and CS_Prenatal_Parental_NoSubUse is that the latter excludes substance abuse (p13) 
 
 
-CS_Prenatal_MH_LifestyleRisks_NoMI <- data.frame(b577a_rec, # In trouble with the law since PREG
+CS_Prenatal_MH_LifestyleRisks_NoMI <- data.frame(IDM, # add mothers ID here,
+                                                 b577a_rec, # In trouble with the law since PREG
                                                  b586a_rec, # PTNR in trouble with law since PREG
                                                  b597a_rec, # Attempted suicide since PREG
                                                  b598a_rec, # Convicted of an offence since PREG
@@ -102,26 +108,77 @@ CS_Prenatal_MH_LifestyleRisks_NoMI <- data.frame(b577a_rec, # In trouble with th
 # 4. INTERPERSONAL RISKS
 
 
-CS_Prenatal_DIS_FamilyRisks <- data.frame(b578a_rec, # Divorced since PREG
-                                          b579a_rec, # PTNR rejected PREG
-                                          b585a_rec, # PTNR went away since PREG		
-                                          b587a_rec, # Separated since PREG		
-                                          b589a_rec, # Argued with PTNR since PREG	
-                                          b590a_rec, # Argued with family or friends since PREG
-                                          b592a_rec, # PTNR hurt mum since PREG	
-                                          b595a_rec, # Got married since PREG		
-                                          b596a_rec, # PTNR hurt CH since PREG	
-                                          b607a_rec, # PTNR was EMOT cruel to mum since PREG
-                                          b608a_rec, # PTNR was EMOT cruel to CH since PREG
-                                          p7, #Partner Status 
-                                          p8, # Partner Affection 
-                                          p9, # Partner cruelty 
-                                          p10, # Family Size
-                                          p11, # Family Major problems 
-                                          p16, # Partner Support
-                                          p17, # Social Network - Emotional 
-                                          p18) # Social Network - Practical 
+CS_Prenatal_InterpersonalRisks <- data.frame(IDM, # add mothers ID here,
+                                             b578a_rec, # Divorced since PREG
+                                             b579a_rec, # PTNR rejected PREG
+                                             b585a_rec, # PTNR went away since PREG		
+                                             b587a_rec, # Separated since PREG		
+                                             b589a_rec, # Argued with PTNR since PREG	
+                                             b590a_rec, # Argued with family or friends since PREG
+                                             b592a_rec, # PTNR hurt mum since PREG	
+                                             b595a_rec, # Got married since PREG		
+                                             b596a_rec, # PTNR hurt CH since PREG	
+                                             b607a_rec, # PTNR was EMOT cruel to mum since PREG
+                                             b608a_rec, # PTNR was EMOT cruel to CH since PREG
+                                             p7, #Partner Status 
+                                             p8, # Partner Affection 
+                                             p9, # Partner cruelty 
+                                             p10, # Family Size
+                                             p11, # Family Major problems 
+                                             p16, # Partner Support
+                                             p17, # Social Network - Emotional 
+                                             p18) # Social Network - Practical 
 
+                                          
+
+####################################################################################################################################################
+
+
+# 5. CREATE PRENATAL CUMULATIVE RISK SCORE 
+
+# This function merges together all prenatal ELS domains according to the IDM column 
+# results in a dataframe with all relevant items for prenatal stress.
+# tech-tip from Serena: use Reduce because merge can only take two dataframes at a time 
+
+CS_CumulativeRisk_Prenatal <- Reduce(function(x,y) merge(x = x, y = y, by = 'IDM',  all.x = TRUE), 
+                                     list(CS_Prenatal_LifeEvents,
+                                          CS_Prenatal_ContextualRisks,
+                                          CS_Prenatal_ParentalRisks,
+                                          CS_Prenatal_InterpersonalRisks))
+
+####################################################################################################################################################
+
+# 6. LINK IDM to IDC 
+
+# In order to later merge the prenatal dataset with postnatal variables, the code below links
+# pregnancy ID (IDM) to the child ID (IDC) 
+
+child_general <- readquick("nameofchilddataset.sav") # read in the data containing mother and child IDs
+child_id <- data.frame(child_general$IDM, child_general$IDC) # extract mother and child IDs
+colnames(child_id) <- c("IDM","IDC") # rename the columns to 'IDM' and 'IDC'
+
+
+# ATTENTION! Only need to run the line below if you are using the prenatal score together with postnatal outcomes/scores. 
+# Note: this will probably change the number of observations
+
+CS_CumulativeRisk_Prenatal <- merge(child_id, CS_CumulativeRisk_Prenatal, by = 'IDM', all.x = T) 
+
+####################################################################################################################################################
+
+# 7. SUMMARY STATISTICS 
+
+# Let's have a look at risk distribution and missing data per indicator (as per Serena's script) 
+# I don't think this section will work with ALSPAC data. We probably will need to alter the code. 
+
+prenatal_summary = data.frame(row.names=c("no risk","risk","NA","%risk","%miss"))
+
+for (i in 3:ncol(CS_CumulativeRisk_Prenatal)) { # ATTENTION, if not merged with child_id, count from 2.
+  s = summary(as.factor(CS_CumulativeRisk_Prenatal[,i]))
+  c = colnames(CS_CumulativeRisk_Prenatal)[i]
+  prenatal_summary[1:3,c] <- s
+  prenatal_summary[4,c] <- round((prenatal_summary[2,c] / 9901)*100, 2)
+  prenatal_summary[5,c] <- round((prenatal_summary[3,c] / 9901)*100, 2)
+}
 
 ####################################################################################################################################################
 
