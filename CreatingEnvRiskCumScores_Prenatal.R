9117a416a217a84e274382e2b707ed96d1070672
Vilte Baltramonaityte <baltramonaityte.vilte@gmail.com>
2021-02-16 12:08:08 +0200

    adding domainscores and some other minor changes

--- CreatingEnvRiskCumScores_Prenatal.R
+++ CreatingEnvRiskCumScores_Prenatal.R
@@ -5,13 +5,23 @@
 
 ####################################################################################################################################################
 
+# The following script builds a dataset with all variables and domain scores used in
+# the creation of prenatal cumulative risk score by Cecil et al. (2014). 
+# It adapts the scripts developed by S. Defina where she created a similar score for Generation R. 
+
+# The script contains the following functions: readquick, percent_missing and domainscore
+# You can find them at SereDef GitHub account (https://github.com/SereDef/cumulative-ELS-score) in 'Setup_and_functions.R'.
+
+####################################################################################################################################################
 
 # 1. LIFE EVENTS
 
+# Steps 1-4 assign variables to the pertinent ELS domains (LE/CR/PR/IR) 
 # depending on the format of the data, you may want to write "attach(nameofdataframe)" and then use the code below. 
 # This will permit the variables to be accessed directly without having to use the dollar sign, as here: 'nameofdata$variablename'
 
-CS_Prenatal_LifeEvents <- data.frame(IDM, # add mothers ID here (find the equivalent term within ALSPAC)
+
+Prenatal_LifeEvents <- data.frame(IDM, # add mothers ID here (find the equivalent term within ALSPAC)
                                      b570a_rec, # PTNR died since PREG
                                      b571a_rec, # CH died since PREG
                                      b572a_rec, # Friend or relative died since PREG
@@ -41,7 +51,7 @@ CS_Prenatal_LifeEvents <- data.frame(IDM, # add mothers ID here (find the equiva
 # 2. CONTEXTUAL RISKS
 
 
-CS_Prenatal_ContextualRisks  <- data.frame(IDM, # add mothers ID here,
+Prenatal_ContextualRisks  <- data.frame(IDM, # add mothers ID here,
                                            b588a_rec, # Income reduced since PREG
                                            b593a_rec, # Became homeless since PREG
                                            b594a_rec, # Major financial PROB since PREG
@@ -57,7 +67,7 @@ CS_Prenatal_ContextualRisks  <- data.frame(IDM, # add mothers ID here,
 
 
 
-CS_Prenatal_ParentalRisks  <- data.frame(IDM, # add mothers ID here,
+Prenatal_ParentalRisks  <- data.frame(IDM, # add mothers ID here,
                                          b577a_rec, # In trouble with the law since PREG
                                          b586a_rec, # PTNR in trouble with law since PREG
                                          b597a_rec, # Attempted suicide since PREG
@@ -73,7 +83,7 @@ CS_Prenatal_ParentalRisks  <- data.frame(IDM, # add mothers ID here,
 # likely suggesting from the three version, this one was selected to form the cumulative score)
 
                                             
-CS_Prenatal_Parental_NoSubUse <- data.frame(IDM, # add mothers ID here,
+Prenatal_Parental_NoSubUse <- data.frame(IDM, # add mothers ID here,
                                             b577a_rec, # In trouble with the law since PREG
                                             b586a_rec, # PTNR in trouble with law since PREG
                                             b597a_rec, # Attempted suicide since PREG
@@ -85,10 +95,10 @@ CS_Prenatal_Parental_NoSubUse <- data.frame(IDM, # add mothers ID here,
                                             p14, # Crime trouble with police 
                                             p15) # Crime convictions   
 
-# The difference between CS_Prenatal_MH_LifestyleRisks and CS_Prenatal_Parental_NoSubUse is that the latter excludes substance abuse (p13) 
+# The difference between Prenatal_MH_LifestyleRisks and Prenatal_Parental_NoSubUse is that the latter excludes substance abuse (p13) 
 
 
-CS_Prenatal_MH_LifestyleRisks_NoMI <- data.frame(IDM, # add mothers ID here,
+Prenatal_MH_LifestyleRisks_NoMI <- data.frame(IDM, # add mothers ID here,
                                                  b577a_rec, # In trouble with the law since PREG
                                                  b586a_rec, # PTNR in trouble with law since PREG
                                                  b597a_rec, # Attempted suicide since PREG
@@ -100,7 +110,7 @@ CS_Prenatal_MH_LifestyleRisks_NoMI <- data.frame(IDM, # add mothers ID here,
                                                  p14, # Crime trouble with police 
                                                  p15) # Crime convictions   
 
-# The difference between CS_Prenatal_MH_LifestyleRisks and CS_Prenatal_MH_LifestyleRisks_NoMI is that the latter excludes psychopathology of mother (p12) 
+# The difference between Prenatal_MH_LifestyleRisks and Prenatal_MH_LifestyleRisks_NoMI is that the latter excludes psychopathology of mother (p12) 
 
 ####################################################################################################################################################
 
@@ -108,7 +118,7 @@ CS_Prenatal_MH_LifestyleRisks_NoMI <- data.frame(IDM, # add mothers ID here,
 # 4. INTERPERSONAL RISKS
 
 
-CS_Prenatal_InterpersonalRisks <- data.frame(IDM, # add mothers ID here,
+Prenatal_InterpersonalRisks <- data.frame(IDM, # add mothers ID here,
                                              b578a_rec, # Divorced since PREG
                                              b579a_rec, # PTNR rejected PREG
                                              b585a_rec, # PTNR went away since PREG		
@@ -140,20 +150,22 @@ CS_Prenatal_InterpersonalRisks <- data.frame(IDM, # add mothers ID here,
 # results in a dataframe with all relevant items for prenatal stress.
 # tech-tip from Serena: use Reduce because merge can only take two dataframes at a time 
 
-CS_CumulativeRisk_Prenatal <- Reduce(function(x,y) merge(x = x, y = y, by = 'IDM',  all.x = TRUE), 
-                                     list(CS_Prenatal_LifeEvents,
-                                          CS_Prenatal_ContextualRisks,
-                                          CS_Prenatal_ParentalRisks,
-                                          CS_Prenatal_InterpersonalRisks))
+prenatal_stress <- Reduce(function(x,y) merge(x = x, y = y, by = 'IDM',  all.x = TRUE), 
+                                     list(Prenatal_LifeEvents,
+                                          Prenatal_ContextualRisks,
+                                          Prenatal_ParentalRisks,
+                                          Prenatal_InterpersonalRisks))
 
 ####################################################################################################################################################
 
 # 6. LINK IDM to IDC 
 
 # In order to later merge the prenatal dataset with postnatal variables, the code below links
-# pregnancy ID (IDM) to the child ID (IDC) 
+# pregnancy ID (IDM) to the child ID (IDC) (according to Esther this will most likely not be required for ALSPAC data)
 
-child_general <- readquick("nameofchilddataset.sav") # read in the data containing mother and child IDs
+child_general <- readquick("nameofchilddataset.sav") # read in the data containing mother and child IDs 
+# if you use the readquick function you need to obtain it from SereDef 'Setup_and_functions.R' script at https://github.com/SereDef/cumulative-ELS-score 
+
 child_id <- data.frame(child_general$IDM, child_general$IDC) # extract mother and child IDs
 colnames(child_id) <- c("IDM","IDC") # rename the columns to 'IDM' and 'IDC'
 
@@ -161,24 +173,157 @@ colnames(child_id) <- c("IDM","IDC") # rename the columns to 'IDM' and 'IDC'
 # ATTENTION! Only need to run the line below if you are using the prenatal score together with postnatal outcomes/scores. 
 # Note: this will probably change the number of observations
 
-CS_CumulativeRisk_Prenatal <- merge(child_id, CS_CumulativeRisk_Prenatal, by = 'IDM', all.x = T) 
+prenatal_stress <- merge(child_id, prenatal_stress, by = 'IDM', all.x = T) 
 
 ####################################################################################################################################################
 
 # 7. SUMMARY STATISTICS 
 
 # Let's have a look at risk distribution and missing data per indicator (as per Serena's script) 
-# I don't think this section will work with ALSPAC data. We probably will need to alter the code. 
+# I don't think this section will work with ALSPAC data. We probably will need to tweak the code. 
 
 prenatal_summary = data.frame(row.names=c("no risk","risk","NA","%risk","%miss"))
 
-for (i in 3:ncol(CS_CumulativeRisk_Prenatal)) { # ATTENTION, if not merged with child_id, count from 2.
-  s = summary(as.factor(CS_CumulativeRisk_Prenatal[,i]))
-  c = colnames(CS_CumulativeRisk_Prenatal)[i]
+for (i in 3:ncol(prenatal_stress)) { # ATTENTION, if not merged with child_id, count from 2.
+  s = summary(as.factor(prenatal_stress[,i]))
+  c = colnames(prenatal_stress)[i]
   prenatal_summary[1:3,c] <- s
   prenatal_summary[4,c] <- round((prenatal_summary[2,c] / 9901)*100, 2)
   prenatal_summary[5,c] <- round((prenatal_summary[3,c] / 9901)*100, 2)
 }
 
 ####################################################################################################################################################
+
+# 8. MISSINGNESS
+
+# Calculate the percentage missing data (obtained from SereDef 'Setup_and_functions.R' script at https://github.com/SereDef/cumulative-ELS-score  
+percent_missing <- function(var) { sum(is.na(var)) / length(var) * 100 }
+
+
+# Apply the percent_missing function to the rows (1) of the entire dataset 
+
+prenatal_stress$pre_percent_missing = apply(prenatal_stress[,3:ncol(prenatal_stress)], # Same as above, if not merged with child_id, count from 2.
+                                            1, percent_missing)
+
+
+####################################################################################################################################################
+
+# 9. CREATE UN-WEIGHTED DOMAIN SCORES 
+
+# ATTENTION! Here we use the default argument of domainscore function: calculating  a 
+# *mean domain score* (range = 0 to 1) that is NOT adjusted for 25% missingness as in 
+# e.g. Rijlaarsdam et al. (2016). If you prefer working with the actual number of risks
+# (i.e. sum score) or the weighted version of it, you can set the argument score_type
+# to 'sum_simple' or 'sum_weighted' respectively (see 'Setup_and_functions.R' script
+# for calculation details at https://github.com/SereDef/cumulative-ELS-score). 
+
+
+# BEFORE RUNNING THE SCRIPT BELOW MAKE SURE YOU OBTAINED THE 'DOMAINSCORE' FUNCTION FROM SEREDEF
+# 'Setup_and_functions.R' script at https://github.com/SereDef/cumulative-ELS-score 
+# Running the domainscore function will add two extra columns to prenatal_stress dataframe: 
+# one containing the % of missingnes per participant and the other containing the domain score 
+
+# LIFE EVENTS
+
+
+prenatal_stress[,c('pre_LE_percent_missing','pre_life_events')] <- domainscore(prenatal_stress[,c(
+  b570a_rec, # PTNR died since PREG
+  b571a_rec, # CH died since PREG
+  b572a_rec, # Friend or relative died since PREG
+  b573a_rec, # CH was ill since PREG	
+  b574a_rec, # PTNR was ill since PREG	
+  b575a_rec, # Friend or relative was ill since PREG
+  b576a_rec, # Admitted to hospital since PREG
+  b580a_rec, # V ill since PREG
+  b581a_rec, # PTNR lost job since PREG
+  b582a_rec, # PTNR had PROBS at work since PREG	
+  b583a_rec, # PROBS at work since PREG
+  b584a_rec, # Lost job since PREG		
+  b591a_rec, # Moved house since PREG	
+  b599a_rec, # Bled & thought might miscarry	
+  b600a_rec, # Started new job since PREG
+  b601a_rec, # Test to see if baby abnormal
+  b602a_rec, # Test result suggesting POSS abnormality	
+  b603a_rec, # Told having twins
+  b604a_rec, # POSS harm to baby
+  b606a_rec, # Took an exam since PREG
+  b609a_rec, # House or car burgled since PREG
+  b610a_rec)]) # Had an accident since PREG
+
+
+                                                                                                                        
+# CONTEXTUAL RISKS
+
+
+prenatal_stress[,c('pre_CR_percent_missing','pre_contextual_risk')] <- domainscore(prenatal_stress[,c(
+  b588a_rec, # Income reduced since PREG
+  b593a_rec, # Became homeless since PREG
+  b594a_rec, # Major financial PROB since PREG
+  p2, # Housing adequacy 
+  p3, # Housing Basic Living 
+  p4, # Housing Defects
+  p6)]) # Financial difficulties 
+
+
+                                                                                                                            
+# PARENTAL RISKS
+  
+
+prenatal_stress[,c('pre_PR_percent_missing','pre_parental_risks')] <- domainscore(prenatal_stress[,c(
+  b577a_rec, # In trouble with the law since PREG
+  b586a_rec, # PTNR in trouble with law since PREG
+  b597a_rec, # Attempted suicide since PREG
+  b598a_rec, # Convicted of an offence since PREG
+  b605a_rec, # Tried to have abortion
+  p1, # Early parenthood
+  p5, # Maternal education 
+  p12, # Psychopathology of mother
+  p13, # Substance abuse 
+  p14, # Crime trouble with police 
+  p15)]) # Crime convictions   
+                                                                                                                            
+
+
+# INTERPERSONAL RISKS
+
+
+prenatal_stress[,c('pre_IS_percent_missing','pre_interpersonal_risks')] <- domainscore(prenatal_stress[,c(
+  b578a_rec, # Divorced since PREG
+  b579a_rec, # PTNR rejected PREG
+  b585a_rec, # PTNR went away since PREG		
+  b587a_rec, # Separated since PREG		
+  b589a_rec, # Argued with PTNR since PREG	
+  b590a_rec, # Argued with family or friends since PREG
+  b592a_rec, # PTNR hurt mum since PREG	
+  b595a_rec, # Got married since PREG		
+  b596a_rec, # PTNR hurt CH since PREG	
+  b607a_rec, # PTNR was EMOT cruel to mum since PREG
+  b608a_rec, # PTNR was EMOT cruel to CH since PREG
+  p7, #Partner Status 
+  p8, # Partner Affection 
+  p9, # Partner cruelty 
+  p10, # Family Size
+  p11, # Family Major problems 
+  p16, # Partner Support
+  p17, # Social Network - Emotional 
+  p18)]) # Social Network - Practical 
+
+                                                                                                                                 
+
+####################################################################################################################################################
+
+# 10. SAVE DATA
+
+# Save the dataset in an .rds file, in the directory where you have the raw data
+saveRDS(prenatal_stress, paste(pathtodata,'prenatal_stress.rds', sep = ""))
+saveRDS(prenatal_summary, paste(pathtodata,'prenatal_stress_summary.rds', sep = ""))
+
+# Or save the dataset in a .csv format, in the directory where you have the raw data
+write.csv(prenatal_stress, file = "prenatal_stress.csv", row.names = FALSE, quote = FALSE)
+write.csv(prenatal_summary, file = "prenatal_stress_summary.csv", row.names = FALSE, quote = FALSE)
+
+# if you used attach(nameofdatafrandata) makes sure to detach the data by typing: detach(nameofdataframe).
+
+####################################################################################################################################################
+
 
